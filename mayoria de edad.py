dff6cabec2d9d617b733f9ebd90a3d2d37dbe516
irvn83933 <irvinmagallanes231@gmail.com>
2026-07-24 17:41:34 -0600

    Add files via upload

---
+++ mayoria de edad.py
@@ -0,0 +1,6 @@
+#verificar la moyoria de edad
+edad= int(input("ingrese su edad: "))
+if edad >= 18:
+    print("eres mayor de edad puedes votar ")
+else:
+    print("no eres mayor de edad no puedes votar ")
